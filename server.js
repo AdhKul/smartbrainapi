20a756b0452a68203590653a3815f2c5077415da
Kuliscene <adhvaith9@gmail.com>
2021-07-12 20:27:54 +0530

    ssl

--- server.js
+++ server.js
@@ -8,13 +8,15 @@ const signIn= require('./controllers/signin.js')
 const profile= require('./controllers/profile.js')
 const image= require('./controllers/image.js')
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED= 0
+
 const db= knex({
 	client: 'pg',
 	connection: {
-		host: '127.0.0.1',
-		user: 'postgres' ,
-		password: 'password',
-		database: 'smartbrain'
+		connectionString: process.env.DATABASE_URL,
+		ssl: {
+			rejectUnauthorezed: false
+		}
 	}
 });
 
